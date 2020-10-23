355c6adf93110b1b2d5fd4286206702fb232b89b
VoronNess <64578357+VoronNess@users.noreply.github.com>
2020-10-24 01:44:00 +0300

    ошибки с evt.key evt.which при модуляции

--- js/main.js
+++ js/main.js
@@ -4,23 +4,7 @@
 // window.card.setCardInfo(createdArray);
 
 window.pageMode.setUnactiveMode();
-const mainPin = document.querySelector(`.map__pin--main`);
-let isEnabledActiveMode = false;
-
-const activeModeEventListener = (evt) => {
-  if (isEnabledActiveMode) {
-    return;
-  }
-  if (evt.which === 1 || evt.key === `Enter`) {
-    window.pageMode.setActiveMode();
-    isEnabledActiveMode = true;
-    mainPin.removeEventListener(`mousedown`, activeModeEventListener);
-    mainPin.removeEventListener(`keydown`, activeModeEventListener);
-  }
-};
-
-mainPin.addEventListener(`mousedown`, activeModeEventListener);
-mainPin.addEventListener(`keydown`, activeModeEventListener);
+window.pin.activeModeEventListener(event);
 
 window.validation.roomElementListener();
 window.validation.guestElementListener();
--- js/pin.js
+++ js/pin.js
@@ -3,8 +3,12 @@
   const pins = document.querySelector(`.map__pins`);
   const pinsTemplate = document.querySelector(`#pin`).content;
   const pinStyle = pinsTemplate.querySelector(`.map__pin`);
+
   const moveX = 100;
   const moveY = 40;
+  const mainPin = document.querySelector(`.map__pin--main`);
+
+  let isEnabledActiveMode = false;
 
   window.pin = {
     renderPins: (array) => {
@@ -20,6 +24,20 @@
         pinImg.alt = array[i].offer.title;
         pins.appendChild(pinElement);
       }
+    },
+
+    activeModeEventListener: (evt) => {
+      if (isEnabledActiveMode) {
+        return;
+      }
+      if (evt.which === 1 || evt.key === `Enter`) {
+        window.pageMode.setActiveMode();
+        isEnabledActiveMode = true;
+        mainPin.removeEventListener(`mousedown`, window.pin.activeModeEventListener(evt));
+        mainPin.removeEventListener(`keydown`, window.pin.activeModeEventListener(evt));
+      }
+      mainPin.addEventListener(`mousedown`, window.pin.activeModeEventListener(evt));
+      mainPin.addEventListener(`keydown`, window.pin.activeModeEventListener(evt));
     }
   };
 })();
